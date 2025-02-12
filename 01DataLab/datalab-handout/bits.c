e5f8151aa25ca9f1fdfbe362bfaef632b5a544c6
jalvlue <1722292195@qq.com>
2025-02-12 19:27:26 +0800

    lab01: finish some of puzzles

--- 01DataLab/datalab-handout/bits.c
+++ 01DataLab/datalab-handout/bits.c
@@ -141,14 +141,14 @@ NOTES:
  *   Max ops: 14
  *   Rating: 1
  */
-int bitXor(int x, int y) { return 2; }
+int bitXor(int x, int y) { return ~(~(x & ~y) & ~(~x & y)); }
 /*
  * tmin - return minimum two's complement integer
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 4
  *   Rating: 1
  */
-int tmin(void) { return 2; }
+int tmin(void) { return 1 << 31; }
 // 2
 /*
  * isTmax - returns 1 if x is the maximum, two's complement number,
@@ -157,7 +157,7 @@ int tmin(void) { return 2; }
  *   Max ops: 10
  *   Rating: 1
  */
-int isTmax(int x) { return 2; }
+int isTmax(int x) { return (!((x + 1) ^ (~x))) & !!(x + 1); }
 /*
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
  *   where bits are numbered from 0 (least significant) to 31 (most significant)
@@ -166,6 +166,7 @@ int isTmax(int x) { return 2; }
  *   Max ops: 12
  *   Rating: 2
  */
+// TODO:
 int allOddBits(int x) { return 2; }
 /*
  * negate - return -x
@@ -174,7 +175,7 @@ int allOddBits(int x) { return 2; }
  *   Max ops: 5
  *   Rating: 2
  */
-int negate(int x) { return 2; }
+int negate(int x) { return ~x + 1; }
 // 3
 /*
  * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0'
@@ -184,7 +185,7 @@ int negate(int x) { return 2; }
  *   Max ops: 15
  *   Rating: 3
  */
-int isAsciiDigit(int x) { return 2; }
+int isAsciiDigit(int x) { return !((x >> 4) ^ 0x3) & !(((x & 0xf) + 6) >> 4); }
 /*
  * conditional - same as x ? y : z
  *   Example: conditional(2,4,5) = 4
